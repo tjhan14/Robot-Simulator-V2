8f887824acbf76142029d045a0778053328c3a43
Taek Jin Han <tj.han@yale.edu>
2021-07-22 15:55:25 -0400

    bonus?

--- index.js
+++ index.js
@@ -3,4 +3,100 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+
+  // target the container in the HTML that we want to add stuff to
+  const movesContainer = document.querySelector("#moves-container")
+
+  // add event listener that responds to when an arrow key is pressed
+  document.addEventListener("keydown", function(event){
+    if (event.key === "ArrowDown"){
+      // create the list element to ADD to the movesContainer
+      const downBox = document.createElement("li")
+      downBox.textContent = "Down"
+
+      // add event listener that deletes box when it is clicked
+
+      downBox.addEventListener("click", function(event){
+        event.target.remove()
+      })
+
+      // add list element to movesContainer
+      movesContainer.append(downBox)
+    }
+    else if (event.key === "ArrowUp"){
+      console.log("ArrowUp")
+      const upBox = document.createElement("li")
+      upBox.textContent = "Up"
+
+      upBox.addEventListener("click", function(event){
+        event.target.remove()
+      })
+
+      movesContainer.append(upBox)
+    }
+    else if (event.key === "ArrowLeft"){
+      console.log("ArrowLeft")
+      const leftBox = document.createElement("li")
+      leftBox.textContent = "Left"
+
+      leftBox.addEventListener("click", function(event){
+        event.target.remove()
+      })
+
+      movesContainer.append(leftBox)
+    }
+    else if (event.key === "ArrowRight"){
+      console.log("ArrowRight")
+      const rightBox = document.createElement("li")
+      rightBox.textContent = "Right"
+
+      rightBox.addEventListener("click", function(event){
+        event.target.remove()
+      })
+
+      movesContainer.append(rightBox)
+    }
+  })
+
+  // target the MOVE button
+  const moveButton = document.querySelector("button#move-button")
+
+  // function to make robot move
+  function moveRobot(movesContainer) {
+    // look at first box inside movesContainer and see what direction
+    const firstBox = movesContainer.firstElementChild
+    // run only if there are boxes inside movesContainer
+    if (firstBox) {
+      if (firstBox.textContent === "Up") {
+        // based on the direction of the box, we tell the robot to move in that direction
+        move("up")
+        // delete that box
+        firstBox.remove()
+      }
+      else if (firstBox.textContent === "Down") {
+        move("down")
+        firstBox.remove()
+      }
+      else if (firstBox.textContent === "Left") {
+        move("left")
+        firstBox.remove()
+      }
+      else if (firstBox.textContent === "Right") {
+        move("right")
+        firstBox.remove()
+      }
+    } else {
+      console.log("There are no boxes!")
+    }
+  }
+
+  // add event listener that responds when the Move button is clicked
+  moveButton.addEventListener('click', function(event) {
+    console.log("Clicked!")
+    while (movesContainer.firstElementChild) {
+      setInterval(function() { moveRobot(movesContainer); }, 1000)
+    }
+  })
+
 });
+
